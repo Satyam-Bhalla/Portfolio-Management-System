3acc764719d331b5ab486ef4c5fd8f07a99ed786
Satyam <satyambhalla17@gmail.com>
2019-04-25 10:36:46 +0530

    Version 12

--- __init__.py
+++ __init__.py
@@ -3,6 +3,7 @@ from itsdangerous import URLSafeTimedSerializer, SignatureExpired
 from datetime import datetime
 import sqlite3
 import base64
+import re
 
 app = Flask(__name__)
 
@@ -15,7 +16,7 @@ c = conn.cursor()
 
 def create_table():
 	c.execute("CREATE TABLE IF NOT EXISTS users (id integer primary key autoincrement, name text, email text, mobile text, password text)")
-	c.execute("CREATE TABLE IF NOT EXISTS details (id integer primary key autoincrement, f_id integer, git text, image text, about text, link integer, foreign key(f_id) references users(id))")
+	c.execute("CREATE TABLE IF NOT EXISTS details (id integer primary key autoincrement, f_id integer, git text, image text, about text,token text, link integer, foreign key(f_id) references users(id))")
 	c.execute("CREATE TABLE IF NOT EXISTS timeline(id integer primary key autoincrement, f_id integer, words text, image text, dateColumn datetime, auth text, foreign key(f_id) references users(id))")
 	c.execute("CREATE TABLE IF NOT EXISTS profile (id integer primary key autoincrement, f_id integer, foreign key(f_id) references users(id))")
 
@@ -68,30 +69,30 @@ def dash():
 	if session['username']:
 		try:
 			k = 5
-			z = 0
 			token = s.dumps(session['username'])
 			url = request.url_root + 'prof/'+ token
 			photo = ""
-			name =""
-			email =""
 			git = ""
-			mob = ""
+			ab = ""
 			conn = sqlite3.connect("data.db")
 			c = conn.cursor()
 			c.execute("SELECT * FROM users WHERE email=?",(session['username'],))
 			new_id = c.fetchone()
+			name = new_id[1]
+			email = new_id[2]
+			mob = new_id[3]
 			c.execute("SELECT * FROM details WHERE f_id=?",(new_id[0],))
 			data = c.fetchone()
 			if data:
-				name = new_id[1]
-				email = new_id[2]
 				git = data[2]
-				mob = new_id[3]
+				ab = data[4]
 				photo = data[3]
-				z = data[5]
-			if request.method == "POST":
+				z = data[6]
+			else:
 				z = 0
-				# return str(request.form['pri'])
+			if request.method == "POST":
+				pic = ""
+				z = 0
 				if 'pri' in request.form:
 					z = 1
 				conn = sqlite3.connect("data.db")
@@ -112,11 +113,12 @@ def dash():
 				if data:
 					c.execute("UPDATE details SET git=?,image=?,about=?, link=?",(request.form['git'],pic,request.form['about'],z))
 				else:
-					c.execute("INSERT INTO details(f_id, git, image, about, link) VALUES(?,?,?,?,?)",(i[0],request.form['git'],pic,request.form['about'],z))
+					c.execute("INSERT INTO details(f_id, git, image, about,token, link) VALUES(?,?,?,?,?,?)",(i[0],request.form['git'],pic,request.form['about'],token,z))
 				conn.commit()
 				return redirect(url_for('dash'))
-			return render_template('dashboard.html',k=k,z=z,url=url,photo=photo,name=name, git=git, mob=mob,email=email)
-		except:
+			return render_template('dashboard.html',ab=ab,k=k,z=z,url=url,photo=photo,name=name, git=git, mob=mob,email=email)
+		except Exception as e:
+			return str(e)
 			return render_template('hidden.html')
 	return redirect(url_for('index'))
 
@@ -136,12 +138,33 @@ def profile(link):
 		git = data[2]
 		mob = new_id[3]
 		photo = data[3]
-		z = data[5]
+		z = data[6]
 		if z == 0:
 			return render_template('dashboard.html',z=z,url=url,photo=photo,name=name, git=git, mob=mob,email=email)
 		return render_template('hidden.html')
 	except:
 		return render_template('hidden.html')
+
+@app.route('/search',methods=['POST'])
+def search():
+	if request.method == 'POST':
+		n = request.form['n']
+		a = re.compile(str(request.form['n']), re.IGNORECASE)
+		doc = []
+		details = []
+		url = request.url_root + 'prof/'
+		conn = sqlite3.connect("data.db")
+		c = conn.cursor()
+		c.execute("SELECT * FROM users")
+		z = c.fetchall()
+		for i in z:
+			b = a.findall(i[1])
+			for j in b:
+				c.execute("SELECT * FROM details WHERE f_id=?",(i[0],))
+				h = c.fetchone()
+				details.append(h)
+				doc.append(i)
+		return render_template('s.html',doc=doc,details=details,url=url)
 
 @app.route('/timeline/', methods=['POST','GET'])
 def timel():
